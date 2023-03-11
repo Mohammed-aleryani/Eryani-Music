3f67fa6d216146849a24a5c02d7ae3db2c2c9b77
Mohammed Aleryni <mym.aleryani@gmail.com>
2023-03-11 01:03:41 +0100

    Implement the given feedback for UI

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const api_key = "api_key=d7342cf1c3cd80aa741d0f7486a48845";
 const dropDown = document.getElementById("dropdown");
 const ul = document.querySelector("#search-results");
+const searchFieldElement = document.getElementById("search-input");
 let searchTimeoutToken = 0;
 
 const fetchData = async (url) => {
@@ -17,26 +18,26 @@ const fetchData = async (url) => {
 
 const renderResult = (data) => {
   dropDown.style.display = "block";
-  let result = data["results"]["artistmatches"]["artist"];
+  const result = data["results"]["artistmatches"]["artist"];
   if (result != "") {
     result.forEach((e) => {
-      let item = document.createElement("li");
+      const item = document.createElement("li");
       item.innerHTML = `${e["name"]}`;
       ul.appendChild(item);
     });
   } else {
-    let item = document.createElement("li");
+    const item = document.createElement("li");
     item.innerHTML = "There is no match result";
     ul.appendChild(item);
   }
 };
 
 const renderInfo = (info) => {
-  let singerInfo = document.getElementById("singer-info");
-  let img = document.getElementsByClassName("card-img-top")[0];
-  let cardTitle = document.getElementsByClassName("card-title")[0];
-  let cardText = document.getElementsByClassName("card-text")[0];
-  let seeMore = document.getElementById("see-more");
+  const singerInfo = document.getElementById("singer-info");
+  const img = document.getElementsByClassName("card-img-top")[0];
+  const cardTitle = document.getElementsByClassName("card-title")[0];
+  const cardText = document.getElementsByClassName("card-text")[0];
+  const seeMore = document.getElementById("see-more");
   singerInfo.style.display = "block";
   img.src = info["artist"]["image"][2]["#text"];
   cardTitle.innerHTML = info["artist"]["name"];
@@ -46,26 +47,25 @@ const renderInfo = (info) => {
 
 const renderError = (error) => {
   dropDown.style.display = "block";
-  let item = document.createElement("li");
+  const item = document.createElement("li");
   item.innerHTML = `${error}`;
   ul.appendChild(item);
 };
 
 const searchResult = () => {
-  const searchFieldElement = document.getElementById("search-input");
-  searchFieldElement.onkeyup = (e) => {
+  searchFieldElement.oninput = (e) => {
     ul.innerHTML = "";
     clearTimeout(searchTimeoutToken);
-    let value = searchFieldElement.value;
+    const value = searchFieldElement.value;
     if (value.trim().length === 0) {
       dropDown.style.display = "none";
       return;
     }
     searchTimeoutToken = setTimeout(async () => {
-      let url = `https://ws.audioscrobbler.com/2.0/?method=artist.search&artist=${encodeURIComponent(
+      const url = `https://ws.audioscrobbler.com/2.0/?method=artist.search&artist=${encodeURIComponent(
         value
       )}&${api_key}&format=json`;
-      let data = await fetchData(url);
+      const data = await fetchData(url);
       if (data["results"]) {
         renderResult(data);
       } else {
@@ -78,10 +78,10 @@ const searchResult = () => {
 const getSingerInfo = async (name) => {
   dropDown.style.display = "none";
 
-  let url = `http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=${encodeURIComponent(
+  const url = `http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=${encodeURIComponent(
     name
   )}&${api_key}&format=json`;
-  let info = await fetchData(url);
+  const info = await fetchData(url);
   renderInfo(info);
 };
 
@@ -90,6 +90,7 @@ window.onload = () => {
 };
 
 ul.addEventListener("click", function (e) {
+ searchFieldElement.value = "";
   if (e.target) {
     getSingerInfo(e.target.innerHTML);
   }
